97719e058859700e4910c6c2cee569047c9558e3
carlton-title <carltonescandor@yahoo.com>
2026-06-18 07:56:51 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'attendance-passport-v1';
+const STATIC_ASSETS = [
+  '/attendancepassport/',
+  '/attendancepassport/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // Always fetch fresh from network, fall back to cache
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
